3f3380b4f94cf425c7579f8b67e90b2f47d460b1
Jose Saldana <jsaldana@unizar.es>
2017-08-28 18:01:15 +0200

    Added shell scripts for generating the figures (#1)
    * shell scripts and README added
    
    * Minor improvement of the README
    
    * README update

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # ns3_802.11_frame_aggregation
 ns3 scripts for simulating scenarios in which 802.11 frame aggregation is studied
+
+## Citation
+If you use this code, please cite the next research article:
+
+Jose Saldana, Jose Ruiz-Mas, Jose Almodovar, "Frame Aggregation in Central Controlled
+802.11 WLANs: the Latency vs. Throughput Trade-off," IEEE Communications Letters,
+accepted for publication, August 2017.
+http://dx.doi.org/10.1109/LCOMM.2017.2741940
+(the citation will be updated as soon as the paper is published in paper)
+
+http://ieeexplore.ieee.org/document/8013762/
+
+Author's self-archive version: http://diec.unizar.es/~jsaldana/personal/amsterdam_2017_in_proc.pdf
+
+
+## Content of the repository
+
+The `.cc` file contains the ns3 script. It has been run with ns3-26 (https://www.nsnam.org/ns-3-26/).
+
+The folder `shell_scripts_used_in_the_paper` contains the files used for obtaining each of
+the figures presented in the paper.
+
+
+## How to use it
+
+- Download ns3.
+
+- Put the `.cc` file in the `ns-3.26/scratch` directory.
+
+- Put a `.sh` file in the `ns-3.26` directory.
+
+- Run a `.sh` file. (You may need to adjust the name of the `.cc` file in the `.sh` script).
---
+++ shell_scripts_used_in_the_paper/test_fig5.4_40-50.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# before running this, delete the files .txt and *_average.txt
+INIT_FILE_NAME="test_fig5.4_40-50"
+
+INIT_NUM_TCP_USERS=40
+STEP_NUM_TCP_USERS=10
+UPPER_LIMIT_NUM_TCP_USERS=50
+
+PERCENTAGE_VOIP_USERS=100 #10 means 10%, i.e. there will be a VoIP upload user per each 10 TCP download users
+
+INITSEED=1
+MAXSEED=20
+
+for ((i=INIT_NUM_TCP_USERS; i<=UPPER_LIMIT_NUM_TCP_USERS; i=$((${i}+${STEP_NUM_TCP_USERS})) )); do
+
+  for ((seed=INITSEED; seed<=MAXSEED; seed++)); do
+
+    NUMBER_TCP_USERS=$i
+
+    NUMBER_VOIP_USERS=$(( (${NUMBER_TCP_USERS}*${PERCENTAGE_VOIP_USERS}) / 100))
+
+    echo "$INIT_FILE_NAME $(date) seed: $seed. number of TCP download users $NUMBER_TCP_USERS. number VoIP upload users $NUMBER_VOIP_USERS. Starting..."
+    NS_GLOBAL_VALUE="RngRun=$seed" ./waf -d optimized --run \
+	"scratch/wifi-central-controlled-aggregation_v139 \
+	--simulationTime=300 \
+	--numberVoIPupload=$NUMBER_VOIP_USERS \
+	--numberVoIPdownload=0 \
+	--numberTCPupload=0 \
+	--numberTCPdownload=$NUMBER_TCP_USERS \
+	--nodeMobility=3 \
+	--number_of_APs=16 \
+	--number_of_APs_per_row=4 \
+	--distance_between_APs=100 \
+	--distanceToBorder=30
+	--outputFileName=$INIT_FILE_NAME \
+	--outputFileSurname="TcpDownUsers-"$NUMBER_TCP_USERS"_seed-"$seed \
+	--rateAPsWithAMPDUenabled=1.0 \
+	--aggregationAlgorithm=1 \
+	--prioritiesEnabled=1 \
+	--powerLevel=1 \
+	#--maxAmpduSize=8000 \
+	#--maxAmpduSizeWhenAggregationDisabled=8000
+	--version80211=0 \
+	--rateModel=Ideal \
+	--RtsCtsThreshold=999999 \
+	--enablePcap=0 \
+	--generateHistograms=0 \
+	--writeMobility=0 \
+	--wifiModel=0 \
+	--numChannels=16 \
+	--verboseLevel=0 \
+	--printSeconds=10 \
+	"
+  done
+done
